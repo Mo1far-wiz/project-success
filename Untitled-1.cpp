dab58e78f97f30c3e633b2c40d3cae7eee3faceb
Molfar <sasha.prohor12@gmail.com>
2020-10-07 16:09:11 +0300

    .\Untitled-1.cpp

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 
+void cout_arr (int*& arr, int& size){
+    for(int q = 0; q < size; ++q){
+        cout << arr[q] << " ";
+    }
+    cout << endl;
+} 
+
 void fill_arr(int*& arr, int size ) {
     for(int i = 0;i<size;i++){
         arr[i] = rand()%50;
